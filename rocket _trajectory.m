85bf777d49defb6c2b4fbac06e619a2b3ea465f2
Theodora Pasare <125205535+TheodoraPasare@users.noreply.github.com>
2025-03-28 19:40:38 +0200

    Update rocket _trajectory.m
    added comments in english

--- rocket _trajectory.m
+++ rocket _trajectory.m
@@ -1,35 +1,35 @@
 clc; clear; close all;
-% toate marimile sunt exprimate in unitati SI
+% all quantities are measured in the international system
 
-% Selectori pentru grafice (1-on; 0-off):
-Gv = 1; % componentele vitezei ca functii de timp
-Gc = 1; % legile de miscare
-Gt = 1; % traiectoria (curba balistica)
-Gd = 1; % reprezentarea dinamica
+% plot selection (1-on; 0-off):
+Gv = 1; % the components of velocity as functions of time
+Gc = 1; % laws of motion
+Gt = 1; % trajectory (the ballistic curve)
+Gd = 1; % dynamical representation
 if Gd==1, Gv=0; Gc=0; Gt=0; end
 
-% Parametrii fizici:
-g = 9.80665; % acceleratia gravitationala
-ro = 7850; % densitatea otelului
-r = 0.13; % raza proiectilului
-m = 4/3 * pi * r^3 * ro; % masa proiectilului
-G = m*g; % greutatea proiectilului
+% Physical parameters:
+g = 9.80665; % gravitational acceleration
+ro = 7850; % density of steel
+r = 0.13; % radius of the projectile
+m = 4/3 * pi * r^3 * ro; % mass of the projectile
+G = m*g; % weight of the projectile
 
-% Conditii initiale:
-v0 = 1100; % viteza initiala
-alpha0 = 43; % unghiul de lansare
-eta = 1.81 * 1e-5; % coeficientul de vascozitate
-b1 = 6 * pi * eta * r; % coeficient termen liniar
-c = 0.469; % coeficient de forma
-ro0 = 1.22; % densitatea aerului
-b2 = c * 4 * pi * r^2 * ro0/2; % coeficient termen patratic
+% Initial conditions:
+v0 = 1100; % initial velocity
+alpha0 = 43; % angle of launch
+eta = 1.81 * 1e-5; % viscosity coefficient
+b1 = 6 * pi * eta * r; % linear term coefficient
+c = 0.469; % shape coefficient
+ro0 = 1.22; % density of air
+b2 = c * 4 * pi * r^2 * ro0/2; % quadratic term coefficient
 
-% Definirea intervalului de timp de inters
+% defining the time
 t0 = 0; tf = 2 * v0 /g*sind(alpha0);
-N = 1500; % numarul momentelor de timp
+N = 1500; % number of moments of time
 t = linspace(t0,tf,N); dt = t(2) - t(1);
 
-% Prealocare si valori de inceput:
+% Starting values:
 vx = zeros(1,N); vy = vx;
 x = zeros(1,N); y = x;
 vx(1) = v0 * cosd(alpha0);
@@ -42,51 +42,51 @@ for i = 1:N-1
     y(i+1)=y(i) + vy(i)*dt;
     if y(i+1)<0, break; end
 end
-t = t(1:i); vx=vx(1:i); vy=vy(1:i); x=x(1:i); y=y(1:i); % eliminarea valorilor in surplus
-if Gv==1 % legile de viteza
+t = t(1:i); vx=vx(1:i); vy=vy(1:i); x=x(1:i); y=y(1:i); % elimination of surplus values
+if Gv==1 % laws of velocity
     figure(1);
     plot(t,vx,'-r',t,vy,'-b');
     xlabel('t(s)'); ylabel('v(m/s)'); grid;
-    title('Componentele vitezei ca functii de timp');
+    title('The components of velocity as functions of time');
     legend('vx','vy');
 end
-if Gc==1 % legile de miscare
+if Gc==1 % laws of motion
     figure(2);
     plot(t,x/1e3,'-r',t,y/1e3,'-b');
     xlabel('t(s)'); ylabel('coord(km)'); grid;
-    title('Coordonatele ca functii de timp');
+    title('Coordinates as functions of time');
     legend('x','y','Location','northwest');
 end
 if Gt==1 % traiectoria
     figure(3);
     plot(x/1e3,y/1e3,'-k','LineWidth',2);
     xlabel('x(km)'); ylabel('y(km)'); grid;
-    title('Curba balistica');
+    title('The ballistic curve');
     axis equal; axis tight;
 end
 
-% Afisarea unor marimi de interes:
-tf = t(i); % timpul de zbor
-b = x(i); % bataia
-h=max(y); % altitudinea maxima
-tu=t(y==h); % timpul de urcare
-tc=tf-tu; % timpul de coborare
-Q=1/2*m*(v0^2-vx(i)^2-vy(i)^2); % caldura produsa prin frecare
-afis=['Timpul de zbor: ', num2str(tf),' s']; disp(afis);
-afis=['Bataia proiectilului: ', num2str(b/1e3),' km']; disp(afis);
-afis=['Altitudinea maxima: ', num2str(h/1e3),' km']; disp(afis);
-afis=['Timpul de urcare: ', num2str(tu),' s']; disp(afis);
-afis=['Timpul de coborare: ', num2str(tc),' s']; disp(afis);
-afis=['Caldura produsa: ', num2str(Q/1e6),' MJ']; disp(afis);
+% Display of quantities of interest:
+tf = t(i); % time of flight
+b = x(i); % projectile range
+h=max(y); % max altitude
+tu=t(y==h); % time of ascent
+tc=tf-tu; % time of descent
+Q=1/2*m*(v0^2-vx(i)^2-vy(i)^2); % heat produced by friction
+afis=['time of flight: ', num2str(tf),' s']; disp(afis);
+afis=['projectile range: ', num2str(b/1e3),' km']; disp(afis);
+afis=['max altitude: ', num2str(h/1e3),' km']; disp(afis);
+afis=['time of ascent: ', num2str(tu),' s']; disp(afis);
+afis=['time of descent: ', num2str(tc),' s']; disp(afis);
+afis=['heat produced by friction: ', num2str(Q/1e6),' MJ']; disp(afis);
 
-if Gd==1 % simularea dinamica
+if Gd==1 % dynamical simulation
     figure(4);
     set(4,'Position',[50 50 850 600]);
-    tic; simt=0; % porneste cronometrul si retine timpul initial
+    tic; simt=0; % starts the timer
     while simt<tfx
         plot(x/1e3,y/1e3,'-c'); hold on;
         xlabel('x(km)'); ylabel('y(km)'); grid;
-        title('Simularea miscarii');
+        title('Motion simulation');
         axis equal; axis tight;
         index=abs(t-simt)==min(abs(t-simt)); % cauta cel mai apropiat t din discretizare
         plot(x(index)/1e3,y(index)/1e3,'.b','MarkerSize',10); hold off
